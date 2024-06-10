9d52d0aa9bbee13829424035636616059c951a85
Rogerio <Rogerio@MACK2>
2024-06-10 15:22:19 -0300

    Terminio do algoritimo que realiza a verificação se um ano é bissexto e se está dentro do calendário gregoriano.

--- Exercicio7/Program.cs
+++ Exercicio7/Program.cs
@@ -4,7 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Vamos verificar se um ano é bissexto!");
+            while (true)
+            {
+                Console.WriteLine("Digite um ano apartir de 1582 com 4 digitos (Exemplo: 2024) ou 999 para sair!");
+                int ano = int.Parse(Console.ReadLine());
+
+                
+                if (ano == 999)
+                {
+                    break;
+                }
+                if ((ano > 1582)) { 
+                    if ((ano % 4) == 0 )
+                    {
+                        if ((ano % 100) == 0)
+                        {
+                            if ((ano % 400) == 0)
+                            {
+                                Console.BackgroundColor = ConsoleColor.Green;
+                                Console.WriteLine("O ano " + ano + " é BISSEXTO");
+                            }
+                            else
+                            {
+                                Console.BackgroundColor = ConsoleColor.Blue;
+                                Console.WriteLine("O ano de " + ano + " não é BISSEXTO!");
+                            }
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Blue;
+                            Console.WriteLine("O ano de " + ano + " não é BISSEXTO!");
+                        }
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("O ano de " + ano + " não é BISSEXTO!");
+                    }
+                }else
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Este ano não está dentro do período do Calendário Gregoriano!");
+                }
+                Console.ResetColor();
+               
+            }
         }
     }
 }
