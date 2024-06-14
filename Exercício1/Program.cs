897746a25d0bb908f380c5464a9d0bbed4f91f11
Rogerio <Rogerio@MACK2>
2024-06-14 13:00:20 -0300

    correção do Exercício

---
+++ Exercício1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercício1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercício 1
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
