454c71a85ec20079141e8769a6dfe3dbd9ec1ab6
Rogerio <Rogerio@MACK2>
2024-06-14 16:07:12 -0300

    Incluido exercício 1 novamente

--- Exercício1/Program.cs
+++ Exercício1/Program.cs
@@ -1,11 +1,42 @@
-﻿namespace Exercício1
+﻿namespace ExerciciosAlgoritmo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Exercício 1
-            Console.WriteLine("Hello World!");
+            // Escreva um programa para que	sejam informados 10 (dez) números inteiros.
+            // O programa deve informar o menor   e o   maior valor   informado.
+
+            int maior = 0; int n = 0; int menor = 0;
+            Console.WriteLine("Digite um número interior: ");
+            int input = int.Parse(Console.ReadLine());
+
+            maior = input;
+            menor = input;
+
+            while (true)
+            {
+
+                Console.WriteLine("Digite um número interior: ");
+                input = int.Parse(Console.ReadLine());
+
+                if (maior <= input)
+                {
+                    maior = input;
+                }
+                if (menor >= input)
+                {
+                    menor = input;
+                }
+                n++;
+                if (n > 10)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("O maior número digitado foi: " + maior);
+            Console.WriteLine("O menor número digitado foi: " + menor);
         }
     }
 }
